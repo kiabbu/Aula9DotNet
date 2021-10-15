480dcc8e6acacf85cb567a82d45fea57cb017155
kiabbu <fsilva.eduardo17@gmail.com>
2021-10-14 21:15:05 -0300

    primeiro exercicio

--- Aula9DotNet/Program.cs
+++ Aula9DotNet/Program.cs
@@ -6,7 +6,31 @@ namespace Aula9DotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! 1");
+            Pessoa pessoa = new Pessoa();
+            Console.WriteLine("Digite o nome: ");
+            pessoa.nome = Console.ReadLine();
+            Console.WriteLine("Digite a idade: ");
+            pessoa.idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o RG: ");
+            pessoa.rg = Console.ReadLine();
+            Console.WriteLine("Digite o CPF: ");
+            pessoa.cpf = Console.ReadLine();
+
+            Console.WriteLine("Nome: {0}", pessoa.nome);
+            Console.WriteLine("Idade: {0}", pessoa.idade);
+            Console.WriteLine("RG: {0}", pessoa.rg);
+            Console.WriteLine("CPF: {0}", pessoa.cpf);
+
+            Pessoa pessoa1 = new Pessoa();
+
+            pessoa1.nome = "Silvia";
+            pessoa1.idade = 39;
+            pessoa1.rg = "111111111";
+            pessoa.cpf = "222222222";
+
+            pessoa.Acenar(pessoa1);
+
+            pessoa.PerguntaIdade(pessoa1);
         }
     }
 }
